819a1fd0e384450b336066814513462afe9cafe3
katspaugh <ivan.kalyaev+github@gmail.com>
2013-02-06 19:47:03 +0400

    set different ports

--- multiplayer/htdocs/app.js
+++ multiplayer/htdocs/app.js
@@ -1,11 +1,14 @@
 (function () {
 	'use strict';
 
+	var WS_PORT = 50092;
+	var host = location.protocol + '//' + location.hostname + ':' + WS_PORT;
+
 	window.addEventListener('load', function () {
 		var wb = new Whiteboard({
 			id: location.hash.substring(1),
 
-			socketHost: 'http://localhost:50089',
+			socketHost: host,
 			publish: '/whiteboard/publish',
 			subscribe: '/whiteboard/subscribe',
 
